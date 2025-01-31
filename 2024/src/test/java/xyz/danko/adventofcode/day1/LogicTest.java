33d83a8b244f35d8480882c24bd4d49aba42ac9d
Thomasda089 <thomasda+github@hotmail.de>
2025-01-31 11:25:45 +0100

    adds logic

--- 2024/src/test/java/xyz/danko/adventofcode/day1/LogicTest.java
+++ 2024/src/test/java/xyz/danko/adventofcode/day1/LogicTest.java
@@ -2,13 +2,23 @@ package xyz.danko.adventofcode.day1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class LogicTest {
 
     @Test
     void getDistance() {
         assertThat(Logic.getDistance()).isEqualTo(11);
+    }
+
+    @Test
+    void getDistanceStream() {
+        var list = List.of(
+                List.of(3,4,2,1,3,3),
+                List.of(4,3,5,3,9,3)
+        );
+        assertThat(Logic.getDistanceStream(list)).isEqualTo(11);
     }
 }
